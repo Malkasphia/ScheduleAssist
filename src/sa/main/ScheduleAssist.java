8838d582abca9d3e716bf32e40ac2e09f287023f
Mal <Mal@Mal-Desktop01>
2018-07-18 21:16:56 -0600

    Separated Database login code to DBConnector class. Refactored DBSelecter to DBLogin to streamline class design.

--- src/sa/main/ScheduleAssist.java
+++ src/sa/main/ScheduleAssist.java
@@ -5,8 +5,14 @@
  */
 package sa.main;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import sa.main.db.*;
 /**
  *
@@ -18,8 +24,9 @@ public class ScheduleAssist {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         //Test DataBase Connection
-        DBSelecter connect = new DBSelecter();
+        DBLogin connect = new DBLogin();
         //Username Login Process
         System.out.println("Welcome to Schedule Assist V 0.1");
         
@@ -33,18 +40,28 @@ public class ScheduleAssist {
     
 
     
-    private static void startLogin (DBSelecter dataBaseQueried) {
+    private static void startLogin (DBLogin dataBaseQueried) {
         //User enters Username
         Scanner scanner = new Scanner (System.in);
         System.out.println("Please enter user login name");
         String userLoginName = scanner.nextLine();
+        System.out.println("Please enter user password");
+        String userPassword = scanner.next();
         //Check Database for User Login. This is done by DBSelector returning the information and checking for a match again the userLoginName
-        dataBaseQueried.userDBGet(userLoginName);
+        boolean restartCheck = dataBaseQueried.userDBGet(userLoginName, userPassword);
+            while (!restartCheck) {
+            System.out.println("Please enter user login name");
+            userLoginName = scanner.next();
+            System.out.println("Please enter user password");
+            userPassword = scanner.next();
+            restartCheck = dataBaseQueried.userDBGet(userLoginName, userPassword);
+            }
         
         
         
     }
     
 
-    
+   
+
 }
